13d966e8c58ee2e9068fe34f7f7b1a61ae81e55d
Thibaut Durand <durand.tibo@gmail.com>
2023-10-07 23:22:54 -0700

    Add `get_package_version` (#8)

--- src/feu/__init__.py
+++ src/feu/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 __all__ = [
+    "get_package_version",
     "is_module_available",
     "is_package_available",
 ]
 
 from feu.imports import is_module_available, is_package_available
+from feu.version import get_package_version
