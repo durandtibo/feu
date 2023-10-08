ed8822b3911a778ba5b4bef8cc5f704140af3698
Thibaut Durand <durand.tibo@gmail.com>
2023-10-07 23:40:14 -0700

    Add `compare_version`

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -1,14 +1,37 @@
-from packaging.version import Version
+from __future__ import annotations
 
-from feu import get_package_version
+import operator
+
+from packaging.version import Version
+from pytest import mark
+
+from feu import compare_version, get_package_version
+
+#####################################
+#     Tests for compare_version     #
+#####################################
+
+
+def test_compare_version_true() -> None:
+    assert compare_version("pytest", operator.ge, "7.3.0")
+
+
+def test_compare_version_false() -> None:
+    assert not compare_version("pytest", operator.le, "7.3.0")
+
+
+def test_compare_version_false_missing() -> None:
+    assert not compare_version("missing", operator.ge, "1.0.0")
+
 
 #########################################
 #     Tests for get_package_version     #
 #########################################
 
 
-def test_get_package_version() -> None:
-    assert isinstance(get_package_version("pytest"), Version)
+@mark.parametrize("package", ("pytest", "ruff"))
+def test_get_package_version(package: str) -> None:
+    assert isinstance(get_package_version(package), Version)
 
 
 def test_get_package_version_missing() -> None:
