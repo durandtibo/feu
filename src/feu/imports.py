7190c2e1e1f0e6bc65047e9aa47e19f878b669c9
Thibaut Durand <durand.tibo@gmail.com>
2024-09-13 23:05:41 -0700

    Add `fire` as optional dependency (#199)

--- src/feu/imports.py
+++ src/feu/imports.py
@@ -2,8 +2,9 @@ r"""Contain to check if a package or module is available."""
 
 from __future__ import annotations
 
-__all__ = ["is_module_available", "is_package_available"]
+__all__ = ["is_module_available", "is_package_available", "check_fire", "is_fire_available"]
 
+from contextlib import suppress
 from functools import lru_cache
 from importlib import import_module
 from importlib.util import find_spec
@@ -33,10 +34,9 @@ def is_package_available(package: str) -> bool:
 
     ```
     """
-    try:
+    with suppress(Exception):
         return find_spec(package) is not None
-    except Exception:  # noqa: BLE001
-        return False
+    return False
 
 
 @lru_cache
@@ -67,3 +67,51 @@ def is_module_available(module: str) -> bool:
     except (ImportError, ModuleNotFoundError):
         return False
     return True
+
+
+################
+#     fire     #
+################
+
+
+@lru_cache
+def is_fire_available() -> bool:
+    r"""Indicate if the ``fire`` package is installed or not.
+
+    Returns:
+        ``True`` if ``fire`` is available otherwise ``False``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from feu.imports import is_fire_available
+    >>> is_fire_available()
+
+    ```
+    """
+    return is_package_available("fire")
+
+
+def check_fire() -> None:
+    r"""Check if the ``fire`` package is installed.
+
+    Raises:
+        RuntimeError: if the ``fire`` package is not installed.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from feu.imports import check_fire
+    >>> check_fire()
+
+    ```
+    """
+    if not is_fire_available():
+        msg = (
+            "'fire' package is required but not installed. "
+            "You can install 'fire' package with the command:\n\n"
+            "pip install fire\n"
+        )
+        raise RuntimeError(msg)
