7b6a67a098c4de8a1399574794ccdab842b82c5c
Thibaut Durand <durand.tibo@gmail.com>
2024-09-18 10:50:53 -0400

    Update CLI entrypoint (#213)

--- src/feu/__main__.py
+++ src/feu/__main__.py
@@ -2,42 +2,91 @@ r"""Contain the main entry point."""
 
 from __future__ import annotations
 
-import sys
-
-from feu.imports import check_fire, is_fire_available
+from feu.imports import is_click_available
 from feu.install import install_package
-from feu.package import _find_closest_version, _is_valid_version
+from feu.package import find_closest_version as find_closest_version_
+from feu.package import is_valid_version
 
-if is_fire_available():  # pragma: no cover
-    import fire
+if is_click_available():  # pragma: no cover
+    import click
 
 
-def main(args: list) -> None:
-    r"""Define the main entry point.
+@click.group()
+def cli() -> None:
+    r"""Implement the main entrypoint."""
+
+
+@click.command()
+@click.option("-n", "--pkg-name", "pkg_name", help="Package name", required=True, type=str)
+@click.option("-v", "--pkg-version", "pkg_version", help="Package version", required=True, type=str)
+def install(pkg_name: str, pkg_version: str) -> None:
+    r"""Install a package and associated packages.
 
     Args:
-        args: The list of input arguments.
+        pkg_name: The package name e.g. ``'pandas'``.
+        pkg_version: The target version to install.
 
     Example usage:
 
-    python -m feu install --package=numpy --version=2.0.2
-    python -m feu check_valid_version --pkg-name=numpy --pkg-version=2.0.2 --python-version=3.11
-    python -m feu find_closest_version --pkg-name=numpy --pkg-version=2.0.2 --python-version=3.11
+    python -m feu install --pkg_name=numpy --pkg_version=2.0.2
     """
-    options = {
-        "install": install_package,
-        "check_valid_version": _is_valid_version,
-        "find_closest_version": _find_closest_version,
-    }
-    opt = args.pop(1)
-    fn = options.get(opt)
-    if fn is None:
-        msg = f"Incorrect argument: {opt}. Valid values are: {sorted(options.keys())}"
-        raise RuntimeError(msg)
-
-    check_fire()
-    fire.Fire(fn)
+    install_package(package=pkg_name, version=pkg_version)
 
 
-if __name__ == "__main__":  # pragma: no cover
-    main(sys.argv)
+@click.command()
+@click.option("-n", "--pkg-name", "pkg_name", help="Package name", required=True, type=str)
+@click.option("-v", "--pkg-version", "pkg_version", help="Package version", required=True, type=str)
+@click.option(
+    "-p", "--python-version", "python_version", help="Python version", required=True, type=str
+)
+def find_closest_version(pkg_name: str, pkg_version: str, python_version: str) -> None:
+    r"""Print the closest valid version given the package name and
+    version, and python version.
+
+    Args:
+        pkg_name: The package name.
+        pkg_version: The package version to check.
+        python_version: The python version.
+
+    Example usage:
+
+    python -m feu find_closest_version --pkg_name=numpy --pkg_version=2.0.2 --python_version=3.10
+    """
+    print(  # noqa: T201
+        find_closest_version_(
+            pkg_name=pkg_name, pkg_version=pkg_version, python_version=python_version
+        )
+    )
+
+
+@click.command()
+@click.option("-n", "--pkg-name", "pkg_name", help="Package name", required=True, type=str)
+@click.option("-v", "--pkg-version", "pkg_version", help="Package version", required=True, type=str)
+@click.option(
+    "-p", "--python-version", "python_version", help="Python version", required=True, type=str
+)
+def check_valid_version(pkg_name: str, pkg_version: str, python_version: str) -> None:
+    r"""Print if the specified package version is valid for the given
+    Python version.
+
+    Args:
+        pkg_name: The package name.
+        pkg_version: The package version to check.
+        python_version: The python version.
+
+    Example usage:
+
+    python -m feu check_valid_version --pkg_name=numpy --pkg_version=2.0.2 --python_version=3.10
+    """
+    print(  # noqa: T201
+        is_valid_version(pkg_name=pkg_name, pkg_version=pkg_version, python_version=python_version)
+    )
+
+
+cli.add_command(install)
+cli.add_command(find_closest_version)
+cli.add_command(check_valid_version)
+
+
+if __name__ == "__main__":
+    cli()
--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -2,45 +2,36 @@ from __future__ import annotations
 
 import subprocess
 from subprocess import CalledProcessError
-from unittest.mock import patch
 
 import pytest
 
-from feu.__main__ import main
 from feu.install import run_bash_command
-from feu.testing import fire_available
+from feu.testing import click_available
 
 ################################
 #     Tests for entrypoint     #
 ################################
 
 
-@fire_available
-def test_main() -> None:
-    args = ["__main__.py", "install", "--package=numpy", "--version=2.0.2"]
-    with patch("sys.argv", args):
-        main(args)
+@click_available
+def test_install() -> None:
+    run_bash_command("python -m feu install --pkg-name=numpy --pkg-version=2.0.2")
 
 
-@fire_available
-def test_main_install() -> None:
-    run_bash_command("python -m feu install --package=numpy --version=2.0.2")
-
-
-@fire_available
-def test_main_check_valid_version() -> None:
+@click_available
+def test_check_valid_version() -> None:
     cmd = (
-        "python -m feu check_valid_version --pkg-name=numpy --pkg-version=2.0.2 "
+        "python -m feu check-valid-version --pkg-name=numpy --pkg-version=2.0.2 "
         "--python-version=3.11"
     )
     out = subprocess.run(cmd.split(), check=True, capture_output=True, text=True)  # noqa: S603
     assert out.stdout == "True\n"
 
 
-@fire_available
-def test_main_find_closest_version() -> None:
+@click_available
+def test_find_closest_version() -> None:
     cmd = (
-        "python -m feu find_closest_version --pkg-name=numpy --pkg-version=2.0.2 "
+        "python -m feu find-closest-version --pkg-name=numpy --pkg-version=2.0.2 "
         "--python-version=3.11"
     )
     out = subprocess.run(cmd.split(), check=True, capture_output=True, text=True)  # noqa: S603
