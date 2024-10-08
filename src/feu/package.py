9c7aa65c93076ec1aeee4eaf3f9a7b8a815fab46
Thibaut Durand <durand.tibo@gmail.com>
2024-10-08 16:33:23 -0700

    Add python 3.13 to workflows

--- src/feu/package.py
+++ src/feu/package.py
@@ -25,6 +25,7 @@ class PackageConfig:
         },
         # https://numpy.org/devdocs/release.html
         "numpy": {
+            "3.13": {"min": "2.1.0", "max": None},
             "3.12": {"min": "1.26.0", "max": None},
             "3.11": {"min": "1.23.2", "max": None},
             "3.10": {"min": "1.21.3", "max": None},
