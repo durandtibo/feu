eaa9704995d4c9ef08046598d3a22c0326cd7355
Thibaut Durand <durand.tibo@gmail.com>
2025-05-01 10:45:11 -0700

    Fix some integration tests

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
+import logging
 import subprocess
 from subprocess import CalledProcessError
 
 import pytest
 
-from feu.install import run_bash_command
+from feu.install import PackageInstaller, run_bash_command
+from feu.package import find_closest_version
 from feu.testing import click_available
+
+logger = logging.getLogger(__name__)
 
 ################################
 #     Tests for entrypoint     #
@@ -15,6 +19,14 @@ from feu.testing import click_available
 
 @click_available
 def test_install() -> None:
+    logger.info(PackageInstaller.registry)
+    logger.info(
+        find_closest_version(
+            pkg_name="numpy",
+            pkg_version="2.2.5",
+            python_version="3.9",
+        )
+    )
     run_bash_command("python -m feu install --pkg-name=numpy --pkg-version=2.2.5")
 
 
