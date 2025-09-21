0c8e587ee9ab882fc65a523d6f0b33b78ede484e
Thibaut Durand <durand.tibo@gmail.com>
2025-09-21 15:43:56 -0700

    Add `UvPackageInstaller`

--- tests/unit/installer/pip/test_installer.py
+++ tests/unit/installer/pip/test_installer.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from unittest.mock import patch
 
-from feu.installer.pip import PipPackageInstaller, PipxPackageInstaller
+from feu.installer.pip import (
+    PipPackageInstaller,
+    PipxPackageInstaller,
+    UvPackageInstaller,
+)
 
 #########################################
 #     Tests for PipPackageInstaller     #
@@ -70,3 +74,37 @@ def test_pipx_package_installer_install_with_args() -> None:
     with patch("feu.installer.pip.installer.run_bash_command") as run_mock:
         installer.install(["numpy==2.0.0"], args="-U")
         run_mock.assert_called_once_with("pipx install -U numpy==2.0.0")
+
+
+########################################
+#     Tests for UvPackageInstaller     #
+########################################
+
+
+def test_uv_package_installer_repr() -> None:
+    assert repr(UvPackageInstaller()).startswith("UvPackageInstaller(")
+
+
+def test_uv_package_installer_str() -> None:
+    assert str(UvPackageInstaller()).startswith("UvPackageInstaller(")
+
+
+def test_uv_package_installer_install() -> None:
+    installer = UvPackageInstaller()
+    with patch("feu.installer.pip.installer.run_bash_command") as run_mock:
+        installer.install(["numpy==2.0.0"])
+        run_mock.assert_called_once_with("uv pip install numpy==2.0.0")
+
+
+def test_uv_package_installer_install_multiple_packages() -> None:
+    installer = UvPackageInstaller()
+    with patch("feu.installer.pip.installer.run_bash_command") as run_mock:
+        installer.install(["numpy==2.0.0", "pandas>=2.0,<3.0"])
+        run_mock.assert_called_once_with("uv pip install numpy==2.0.0 pandas>=2.0,<3.0")
+
+
+def test_uv_package_installer_install_with_args() -> None:
+    installer = UvPackageInstaller()
+    with patch("feu.installer.pip.installer.run_bash_command") as run_mock:
+        installer.install(["numpy==2.0.0"], args="-U")
+        run_mock.assert_called_once_with("uv pip install -U numpy==2.0.0")
