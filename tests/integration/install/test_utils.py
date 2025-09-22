04eb6cd696d3be707b190bc184872f8769a238c8
Thibaut Durand <durand.tibo@gmail.com>
2025-09-22 01:34:17 -0700

    Add integration test for installers (#472)

--- tests/integration/install/test_utils.py
+++ tests/integration/install/test_utils.py
@@ -8,17 +8,22 @@ from feu import is_package_available
 from feu.installer import get_available_installers, install_package
 from feu.package import find_closest_version
 
-PACKAGES = [("numpy", "2.2.5")]
-
 
 @pytest.fixture(autouse=True)
 def _reset_cache() -> None:
     is_package_available.cache_clear()
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def python_version() -> str:
     return f"{version_info.major}.{version_info.minor}"
+
+
+@pytest.fixture(scope="module")
+def numpy_version(python_version: str) -> str:
+    return find_closest_version(
+        pkg_name="numpy", pkg_version="2.2.5", python_version=python_version
+    )
 
 
 #####################################
@@ -27,10 +32,6 @@ def python_version() -> str:
 
 
 @pytest.mark.parametrize("installer", get_available_installers())
-@pytest.mark.parametrize(("package", "version"), PACKAGES)
-def test_install_package(installer: str, package: str, version: str, python_version: str) -> None:
-    version = find_closest_version(
-        pkg_name=package, pkg_version=version, python_version=python_version
-    )
-    install_package(installer=installer, package=package, version=version)
-    assert is_package_available(package)
+def test_install_package(installer: str, numpy_version: str) -> None:
+    install_package(installer=installer, package="numpy", version=numpy_version)
+    assert is_package_available("numpy")
