41f28e67e8ce487ab1f8673c45560d2b7954bd98
Thibaut Durand <durand.tibo@gmail.com>
2025-09-22 14:46:40 -0700

    Add `filter_stable_versions` (#476)

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -6,6 +6,7 @@ import pytest
 from packaging.version import Version
 
 from feu import compare_version, get_package_version
+from feu.version import filter_stable_versions
 
 #####################################
 #     Tests for compare_version     #
@@ -36,3 +37,40 @@ def test_get_package_version(package: str) -> None:
 
 def test_get_package_version_missing() -> None:
     assert get_package_version("missing") is None
+
+
+############################################
+#     Tests for filter_stable_versions     #
+############################################
+
+
+def test_filter_stable_versions() -> None:
+    assert filter_stable_versions(["1.0.0", "1.0.0a1", "2.0.0", "2.0.0.dev1", "3.0.0.post1"]) == [
+        "1.0.0",
+        "2.0.0",
+    ]
+
+
+def test_filter_stable_versions_all() -> None:
+    assert filter_stable_versions(["1.0.0", "2.0.1", "3.3.3", "0.9"]) == [
+        "1.0.0",
+        "2.0.1",
+        "3.3.3",
+        "0.9",
+    ]
+
+
+def test_filter_stable_versions_prereleases() -> None:
+    assert filter_stable_versions(["1.0.0", "2.0.0a1", "2.0.0b2", "2.0.0rc3"]) == ["1.0.0"]
+
+
+def test_filter_stable_versions_postreleases() -> None:
+    assert filter_stable_versions(["1.0.0", "1.0.0.post1", "2.0.0.post2"]) == ["1.0.0"]
+
+
+def test_filter_stable_versions_devreleases() -> None:
+    assert filter_stable_versions(["1.0.0", "1.0.0.dev1", "2.0.0.dev2"]) == ["1.0.0"]
+
+
+def test_filter_stable_versions_empty() -> None:
+    assert filter_stable_versions([]) == []
