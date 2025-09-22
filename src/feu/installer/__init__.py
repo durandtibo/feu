b63233ac9d23ac629c106a38fc438e2de894b725
Thibaut Durand <durand.tibo@gmail.com>
2025-09-22 00:14:51 -0700

    Add `InstallerRegistry` (#454)

--- src/feu/installer/__init__.py
+++ src/feu/installer/__init__.py
@@ -2,6 +2,7 @@ r"""Contain package installers."""
 
 from __future__ import annotations
 
-__all__ = ["BaseInstaller"]
+__all__ = ["BaseInstaller", "InstallerRegistry"]
 
 from feu.installer.installer import BaseInstaller
+from feu.installer.registry import InstallerRegistry
