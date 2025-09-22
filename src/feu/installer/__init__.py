c7ab9f99b685450a96363d061f038245d28e978e
Thibaut Durand <durand.tibo@gmail.com>
2025-09-22 00:36:59 -0700

    Add `is_pip_available`

--- src/feu/installer/__init__.py
+++ src/feu/installer/__init__.py
@@ -2,8 +2,8 @@ r"""Contain package installers."""
 
 from __future__ import annotations
 
-__all__ = ["BaseInstaller", "InstallerRegistry", "install_package"]
+__all__ = ["BaseInstaller", "InstallerRegistry", "install_package", "is_pip_available"]
 
 from feu.installer.installer import BaseInstaller
 from feu.installer.registry import InstallerRegistry
-from feu.installer.utils import install_package
+from feu.installer.utils import install_package, is_pip_available
