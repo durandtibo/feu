eb25ff0b07d62d13b6b685b819a5fb1d3198971a
Thibaut Durand <durand.tibo@gmail.com>
2025-09-22 00:30:22 -0700

    Add integration test for new `install_package`

---
+++ tests/integration/install/test_utils.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from sys import version_info
+
+import pytest
+
+from feu import is_package_available
+from feu.installer import install_package
+from feu.package import find_closest_version
+
+PACKAGES = [
+    ("numpy", "2.2.5"),
+    ("pandas", "2.2.3"),
+    ("sklearn", "1.6.0"),
+    ("xarray", "2024.9"),
+]
+
+
+@pytest.fixture(autouse=True)
+def _reset_cache() -> None:
+    is_package_available.cache_clear()
+
+
+@pytest.fixture
+def python_version() -> str:
+    return f"{version_info.major}.{version_info.minor}"
+
+
+#####################################
+#     Tests for install_package     #
+#####################################
+
+
+@pytest.mark.parametrize(("package", "version"), PACKAGES)
+def test_install_package(installer: str, package: str, version: str, python_version: str) -> None:
+    version = find_closest_version(
+        pkg_name=package, pkg_version=version, python_version=python_version
+    )
+    install_package(installer=installer, package=package, version=version)
+    assert is_package_available(package)
