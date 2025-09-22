8d698a8bb4a1fbeaddf9ad731812bc5beba175d4
Thibaut Durand <durand.tibo@gmail.com>
2025-09-22 16:05:08 -0700

    Add `filter_valid_versions` (#477)

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -6,7 +6,7 @@ import pytest
 from packaging.version import Version
 
 from feu import compare_version, get_package_version
-from feu.version import filter_stable_versions
+from feu.version import filter_stable_versions, filter_valid_versions
 
 #####################################
 #     Tests for compare_version     #
@@ -52,11 +52,24 @@ def test_filter_stable_versions() -> None:
 
 
 def test_filter_stable_versions_all() -> None:
-    assert filter_stable_versions(["1.0.0", "2.0.1", "3.3.3", "0.9"]) == [
+    assert filter_stable_versions(
+        [
+            "1.0.0",
+            "2.0.1",
+            "3.3.3",
+            "0.9",
+            "v1.0.0",
+            "2024.6",
+            "2024.07",
+        ]
+    ) == [
         "1.0.0",
         "2.0.1",
         "3.3.3",
         "0.9",
+        "v1.0.0",
+        "2024.6",
+        "2024.07",
     ]
 
 
@@ -74,3 +87,62 @@ def test_filter_stable_versions_devreleases() -> None:
 
 def test_filter_stable_versions_empty() -> None:
     assert filter_stable_versions([]) == []
+
+
+###########################################
+#     Tests for filter_valid_versions     #
+###########################################
+
+
+def test_filter_valid_versions() -> None:
+    assert filter_valid_versions(
+        [
+            "1.0.0",
+            "1.0.0a1",
+            "2.0.0.post1",
+            "not-a-version",
+            "",
+            "2",
+            "3.0",
+            "v1.0.0",
+            "1.0.0.0.0",
+            "4.0.0.dev1",
+            "2024.6",
+        ]
+    ) == [
+        "1.0.0",
+        "1.0.0a1",
+        "2.0.0.post1",
+        "2",
+        "3.0",
+        "v1.0.0",
+        "1.0.0.0.0",
+        "4.0.0.dev1",
+        "2024.6",
+    ]
+
+
+def test_filter_valid_versions_all_valid() -> None:
+    assert filter_valid_versions(["1.0.0", "2.0.0a1", "2.1.3.post1", "0.1.dev2", "3.0"]) == [
+        "1.0.0",
+        "2.0.0a1",
+        "2.1.3.post1",
+        "0.1.dev2",
+        "3.0",
+    ]
+
+
+def test_filter_valid_versions_all_invalid() -> None:
+    assert filter_valid_versions(["not-a-version", "", "abc", "invalid"]) == []
+
+
+def test_filter_valid_versions_mixed() -> None:
+    assert filter_valid_versions(["1.0.0", "invalid", "", "2024.6", "!!", "2.0"]) == [
+        "1.0.0",
+        "2024.6",
+        "2.0",
+    ]
+
+
+def test_filter_valid_versions_empty() -> None:
+    assert filter_valid_versions([]) == []
