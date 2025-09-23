de86c6b497cc75d85a64f2f92ff4ba478dedf256
Thibaut Durand <durand.tibo@gmail.com>
2025-09-23 00:26:20 -0700

    Add `get_python_major_minor` (#489)

--- tests/integration/install/test_utils.py
+++ tests/integration/install/test_utils.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-from sys import version_info
-
 import pytest
 
 from feu import is_package_available
 from feu.install import get_available_installers, install_package
 from feu.package import find_closest_version
+from feu.utils.version import get_python_major_minor
 
 
 @pytest.fixture(autouse=True)
@@ -15,14 +14,9 @@ def _reset_cache() -> None:
 
 
 @pytest.fixture(scope="module")
-def python_version() -> str:
-    return f"{version_info.major}.{version_info.minor}"
-
-
-@pytest.fixture(scope="module")
-def numpy_version(python_version: str) -> str:
+def numpy_version() -> str:
     return find_closest_version(
-        pkg_name="numpy", pkg_version="2.2.5", python_version=python_version
+        pkg_name="numpy", pkg_version="2.2.5", python_version=get_python_major_minor()
     )
 
 
