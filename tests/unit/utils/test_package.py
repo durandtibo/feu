ff64fc485e17c45f24d113d2ad12d6913209fccb
Thibaut Durand <durand.tibo@gmail.com>
2025-09-26 07:45:10 -0700

    Add `extract_package_extras`

--- tests/unit/utils/test_package.py
+++ tests/unit/utils/test_package.py
@@ -4,10 +4,15 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from feu.utils.package import extract_package_name, generate_extras_string
+from feu.utils.package import (
+    extract_package_extras,
+    extract_package_name,
+    generate_extras_string,
+)
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
+
 
 ##########################################
 #     Tests for extract_package_name     #
@@ -17,14 +22,32 @@ if TYPE_CHECKING:
 @pytest.mark.parametrize(
     ("requirement", "expected"),
     [
-        ("requests[security,socks]", "requests"),
         ("numpy", "numpy"),
         ("pandas[performance]", "pandas"),
+        ("requests[security,socks]", "requests"),
         ("some-package[extra1,extra2]", "some-package"),
     ],
 )
 def test_extract_package_name(requirement: str, expected: str) -> None:
     assert extract_package_name(requirement) == expected
+
+
+############################################
+#     Tests for extract_package_extras     #
+############################################
+
+
+@pytest.mark.parametrize(
+    ("requirement", "expected"),
+    [
+        ("numpy", []),
+        ("pandas[performance]", ["performance"]),
+        ("requests[security,socks]", ["security", "socks"]),
+        ("some-package[extra1,extra2]", ["extra1", "extra2"]),
+    ],
+)
+def test_extract_package_extras(requirement: str, expected: str) -> None:
+    assert extract_package_extras(requirement) == expected
 
 
 ############################################
