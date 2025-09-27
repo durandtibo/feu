44e14acb0b8b86dc4fdde7af63065b69b13d962f
Thibaut Durand <durand.tibo@gmail.com>
2025-09-27 03:13:55 -0700

    Add new `JaxDependencyResolver` (#505)

--- src/feu/install/pip/resolver2.py
+++ src/feu/install/pip/resolver2.py
@@ -5,20 +5,26 @@ from __future__ import annotations
 __all__ = [
     "BaseDependencyResolver",
     "DependencyResolver",
+    "JaxDependencyResolver",
 ]
 
 import logging
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Any
 
+from packaging.version import Version
+
+from feu.utils.package import PackageDependency
+
 if TYPE_CHECKING:
-    from feu.utils.package import Package, PackageDependency
+    from feu.utils.package import Package
 
 logger = logging.getLogger(__name__)
 
 
 class BaseDependencyResolver(ABC):
-    r"""Define the base class for pip-compatible package dependency resolvers.
+    r"""Define the base class for pip-compatible package dependency
+    resolvers.
 
     Example usage:
 
@@ -115,3 +121,37 @@ class DependencyResolver(BaseDependencyResolver):
 
     def resolve(self, package: Package) -> list[PackageDependency]:
         return [package.to_package_dependency()]
+
+
+class JaxDependencyResolver(DependencyResolver):
+    r"""Implement the ``jax`` dependency resolver.
+
+    ``numpy`` 2.0 support was added in ``jax`` 0.4.26.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from feu.install.pip.resolver2 import JaxDependencyResolver
+    >>> from feu.utils.package import Package
+    >>> resolver = JaxDependencyResolver()
+    >>> resolver
+    JaxDependencyResolver()
+    >>> deps = resolver.resolve(Package(name="jax", version="0.4.26"))
+    >>> deps
+    [PackageDependency(name='jax', version_specifiers=['==0.4.26'], extras=None),
+     PackageDependency(name='jaxlib', version_specifiers=['==0.4.26'], extras=None)]
+
+    ```
+    """
+
+    def resolve(self, package: Package) -> list[PackageDependency]:
+        deps = super().resolve(package)
+        deps.append(PackageDependency("jaxlib", version_specifiers=[f"=={package.version}"]))
+        ver = Version(package.version)
+        if ver < Version("0.4.26"):
+            deps.append(PackageDependency("numpy", version_specifiers=["<2.0.0"]))
+        if Version("0.4.9") <= ver <= Version("0.4.11"):
+            # https://github.com/google/jax/issues/17693
+            deps.append(PackageDependency("ml_dtypes", version_specifiers=["<=0.2.0"]))
+        return deps
--- tests/unit/install/pip/test_resolver2.py
+++ tests/unit/install/pip/test_resolver2.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from feu.install.pip.resolver2 import (
-    DependencyResolver,
-)
+import pytest
+
+from feu.install.pip.resolver2 import DependencyResolver, JaxDependencyResolver
 from feu.utils.package import Package, PackageDependency
 
 ########################################
@@ -47,4 +47,66 @@ def test_dependency_resolver_resolve_with_extras() -> None:
 def test_dependency_resolver_resolve_with_extras_empty() -> None:
     assert DependencyResolver().resolve(Package(name="my_package", version="1.2.3", extras=[])) == [
         PackageDependency(name="my_package", version_specifiers=["==1.2.3"], extras=[])
+    ]
+
+
+###########################################
+#     Tests for JaxDependencyResolver     #
+###########################################
+
+
+def test_jax_dependency_resolver_repr() -> None:
+    assert repr(JaxDependencyResolver()).startswith("JaxDependencyResolver(")
+
+
+def test_jax_dependency_resolver_str() -> None:
+    assert str(JaxDependencyResolver()).startswith("JaxDependencyResolver(")
+
+
+def test_jax_dependency_resolver_equal_true() -> None:
+    assert JaxDependencyResolver().equal(JaxDependencyResolver())
+
+
+def test_jax_dependency_resolver_equal_false() -> None:
+    assert not JaxDependencyResolver().equal(42)
+
+
+def test_jax_dependency_resolver_resolve() -> None:
+    assert JaxDependencyResolver().resolve(Package(name="jax", version="0.4.26")) == [
+        PackageDependency(name="jax", version_specifiers=["==0.4.26"]),
+        PackageDependency(name="jaxlib", version_specifiers=["==0.4.26"]),
+    ]
+
+
+def test_jax_dependency_resolver_resolve_high() -> None:
+    assert JaxDependencyResolver().resolve(Package(name="jax", version="0.4.27")) == [
+        PackageDependency(name="jax", version_specifiers=["==0.4.27"]),
+        PackageDependency(name="jaxlib", version_specifiers=["==0.4.27"]),
+    ]
+
+
+def test_jax_dependency_resolver_resolve_low() -> None:
+    assert JaxDependencyResolver().resolve(Package(name="jax", version="0.4.25")) == [
+        PackageDependency(name="jax", version_specifiers=["==0.4.25"]),
+        PackageDependency(name="jaxlib", version_specifiers=["==0.4.25"]),
+        PackageDependency(name="numpy", version_specifiers=["<2.0.0"]),
+    ]
+
+
+@pytest.mark.parametrize("version", ["0.4.9", "0.4.10", "0.4.11"])
+def test_jax_dependency_resolver_resolve_ml_dtypes(version: str) -> None:
+    assert JaxDependencyResolver().resolve(Package(name="jax", version=version)) == [
+        PackageDependency(name="jax", version_specifiers=[f"=={version}"]),
+        PackageDependency(name="jaxlib", version_specifiers=[f"=={version}"]),
+        PackageDependency(name="numpy", version_specifiers=["<2.0.0"]),
+        PackageDependency(name="ml_dtypes", version_specifiers=["<=0.2.0"]),
+    ]
+
+
+def test_jax_dependency_resolver_resolve_with_extras() -> None:
+    assert JaxDependencyResolver().resolve(
+        Package(name="jax", version="0.4.26", extras=["dev"])
+    ) == [
+        PackageDependency(name="jax", version_specifiers=["==0.4.26"], extras=["dev"]),
+        PackageDependency(name="jaxlib", version_specifiers=["==0.4.26"]),
     ]
