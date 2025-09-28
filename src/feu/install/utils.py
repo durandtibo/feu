59938d4a75ac6300f3af4feadac316998c2c5669
Thibaut Durand <durand.tibo@gmail.com>
2025-09-28 08:41:48 -0700

    Update `install_package`

--- src/feu/install/utils.py
+++ src/feu/install/utils.py
@@ -75,14 +75,16 @@ def install_package_closest_version(installer: InstallerSpec, package: PackageSp
 
     ```
     """
-    package.with_version(
-        find_closest_version(
-            pkg_name=package.name,
-            pkg_version=package.version,
-            python_version=get_python_major_minor(),
-        )
+    install_package(
+        installer=installer,
+        package=package.with_version(
+            find_closest_version(
+                pkg_name=package.name,
+                pkg_version=package.version,
+                python_version=get_python_major_minor(),
+            )
+        ),
     )
-    install_package(installer=installer, package=package)
 
 
 @lru_cache(1)
