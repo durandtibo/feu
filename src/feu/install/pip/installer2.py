d06d3a79a9a2c87c8a649343308526de934c05b0
Thibaut Durand <durand.tibo@gmail.com>
2025-09-30 00:58:33 -0700

    Add new `PipInstaller`

--- src/feu/install/pip/installer2.py
+++ src/feu/install/pip/installer2.py
@@ -34,7 +34,6 @@ class BasePipInstaller(BaseInstaller):
     def install(self, package: PackageSpec) -> None:
         deps = DependencyResolverRegistry.find_resolver(package).resolve(package)
         cmd = self._generate_command(deps=deps, args=self._arguments)
-        cmd = " ".join(cmd.split())  # remove duplicate spaces
         run_bash_command(cmd)
 
     @abstractmethod
