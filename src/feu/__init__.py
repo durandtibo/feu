d1ffcfcc4f12787dd5d161ad65bf274c36ca278c
Thibaut Durand <durand.tibo@gmail.com>
2025-10-03 14:20:54 -0700

    Add `versio.comparison` module

--- src/feu/__init__.py
+++ src/feu/__init__.py
@@ -6,10 +6,11 @@ __all__ = [
     "compare_version",
     "get_package_version",
     "install_package",
+    "install_package_closest_version",
     "is_module_available",
     "is_package_available",
 ]
 
 from feu.imports import is_module_available, is_package_available
-from feu.install import install_package
+from feu.install import install_package, install_package_closest_version
 from feu.version import compare_version, get_package_version
---
+++ src/feu/versio/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain functions to compare package versions."""
+
+from __future__ import annotations
+
+__all__ = ["compare_version"]
+
+from feu.versio.comparison import compare_version
