883a0e04c3b5897a42f3d60e5295c476ed0618c0
Thibaut Durand <durand.tibo@gmail.com>
2025-10-03 14:40:30 -0700

    Add `versio.filtering` module

--- src/feu/versio/__init__.py
+++ src/feu/versio/__init__.py
@@ -2,7 +2,14 @@ r"""Contain functions to compare package versions."""
 
 from __future__ import annotations
 
-__all__ = ["compare_version", "get_package_version", "get_python_major_minor"]
+__all__ = [
+    "compare_version",
+    "filter_stable_versions",
+    "filter_valid_versions",
+    "get_package_version",
+    "get_python_major_minor",
+]
 
 from feu.versio.comparison import compare_version
+from feu.versio.filtering import filter_stable_versions, filter_valid_versions
 from feu.versio.runtime import get_package_version, get_python_major_minor
