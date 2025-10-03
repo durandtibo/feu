9560a1be08ef0d34a8e81337794f4a9b91009eab
Thibaut Durand <durand.tibo@gmail.com>
2025-10-03 14:33:56 -0700

    Add `versio.runtime` module

--- src/feu/versio/__init__.py
+++ src/feu/versio/__init__.py
@@ -2,6 +2,7 @@ r"""Contain functions to compare package versions."""
 
 from __future__ import annotations
 
-__all__ = ["compare_version"]
+__all__ = ["compare_version", "get_package_version", "get_python_major_minor"]
 
 from feu.versio.comparison import compare_version
+from feu.versio.runtime import get_package_version, get_python_major_minor
