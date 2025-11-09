d09dac3b61e9061971af2ebc45b30c9ba16505e3
Thibaut Durand <durand.tibo@gmail.com>
2025-11-09 10:12:49 -0800

    Add `get_versions`

--- tests/integration/version/test_package.py
+++ tests/integration/version/test_package.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from feu.testing import requests_available
 from feu.version import (
     get_latest_major_versions,
     get_latest_minor_versions,
@@ -11,6 +12,7 @@ from feu.version import (
 ##################################
 
 
+@requests_available
 def test_get_versions_requests() -> None:
     assert get_versions("requests", lower="2.25", upper="2.30") == (
         "2.25.0",
@@ -25,6 +27,7 @@ def test_get_versions_requests() -> None:
     )
 
 
+@requests_available
 def test_get_versions_torch() -> None:
     assert get_versions("torch", lower="2.5", upper="2.9") == (
         "2.5.0",
@@ -41,10 +44,12 @@ def test_get_versions_torch() -> None:
 ###############################################
 
 
+@requests_available
 def test_get_latest_major_versions_requests() -> None:
     assert get_latest_major_versions("requests", upper="2.30") == ("0.14.2", "1.2.3", "2.29.0")
 
 
+@requests_available
 def test_get_latest_major_versions_torch() -> None:
     assert get_latest_major_versions("torch", upper="2.9") == ("1.13.1", "2.8.0")
 
@@ -54,6 +59,7 @@ def test_get_latest_major_versions_torch() -> None:
 ###############################################
 
 
+@requests_available
 def test_get_latest_minor_versions_requests() -> None:
     assert get_latest_minor_versions("requests", lower="2.10", upper="2.30") == (
         "2.10.0",
@@ -79,6 +85,7 @@ def test_get_latest_minor_versions_requests() -> None:
     )
 
 
+@requests_available
 def test_get_latest_minor_versions_torch() -> None:
     assert get_latest_minor_versions("torch", lower="2.0", upper="2.9") == (
         "2.0.1",
--- tests/unit/version/test_package.py
+++ tests/unit/version/test_package.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from unittest.mock import Mock, patch
 
+from feu.testing import requests_available
 from feu.version import (
     get_latest_major_versions,
     get_latest_minor_versions,
@@ -13,24 +14,28 @@ from feu.version import (
 ##################################
 
 
+@requests_available
 def test_get_versions() -> None:
     mock = Mock(return_value=("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_versions("my_package") == ("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3")
 
 
+@requests_available
 def test_get_versions_lower() -> None:
     mock = Mock(return_value=("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_versions("my_package", lower="1.1.0") == ("1.1.0", "1.1.2", "2.0.0", "2.0.3")
 
 
+@requests_available
 def test_get_versions_upper() -> None:
     mock = Mock(return_value=("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_versions("my_package", upper="2.0.0") == ("1.0.0", "1.0.1", "1.1.0", "1.1.2")
 
 
+@requests_available
 def test_get_versions_range() -> None:
     mock = Mock(return_value=("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3"))
     with patch("feu.version.package.get_pypi_versions", mock):
@@ -42,24 +47,28 @@ def test_get_versions_range() -> None:
 ###############################################
 
 
+@requests_available
 def test_get_latest_major_versions() -> None:
     mock = Mock(return_value=("0.1.0", "0.8.0", "0.9.0", "1.0.0", "1.2.0", "1.3.0", "2.0.0"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_latest_major_versions("my_package") == ("0.9.0", "1.3.0", "2.0.0")
 
 
+@requests_available
 def test_get_latest_major_versions_lower() -> None:
     mock = Mock(return_value=("0.1.0", "0.8.0", "0.9.0", "1.0.0", "1.2.0", "1.3.0", "2.0.0"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_latest_major_versions("my_package", lower="1.0.0") == ("1.3.0", "2.0.0")
 
 
+@requests_available
 def test_get_latest_major_versions_upper() -> None:
     mock = Mock(return_value=("0.1.0", "0.8.0", "0.9.0", "1.0.0", "1.2.0", "1.3.0", "2.0.0"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_latest_major_versions("my_package", upper="2.0.0") == ("0.9.0", "1.3.0")
 
 
+@requests_available
 def test_get_latest_major_versions_range() -> None:
     mock = Mock(return_value=("0.1.0", "0.8.0", "0.9.0", "1.0.0", "1.2.0", "1.3.0", "2.0.0"))
     with patch("feu.version.package.get_pypi_versions", mock):
@@ -71,24 +80,28 @@ def test_get_latest_major_versions_range() -> None:
 ###############################################
 
 
+@requests_available
 def test_get_latest_minor_versions() -> None:
     mock = Mock(return_value=("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_latest_minor_versions("my_package") == ("1.0.1", "1.1.2", "2.0.3")
 
 
+@requests_available
 def test_get_latest_minor_versions_lower() -> None:
     mock = Mock(return_value=("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_latest_minor_versions("my_package", lower="1.1.0") == ("1.1.2", "2.0.3")
 
 
+@requests_available
 def test_get_latest_minor_versions_upper() -> None:
     mock = Mock(return_value=("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3"))
     with patch("feu.version.package.get_pypi_versions", mock):
         assert get_latest_minor_versions("my_package", upper="2.0.0") == ("1.0.1", "1.1.2")
 
 
+@requests_available
 def test_get_latest_minor_versions_range() -> None:
     mock = Mock(return_value=("1.0.0", "1.0.1", "1.1.0", "1.1.2", "2.0.0", "2.0.3"))
     with patch("feu.version.package.get_pypi_versions", mock):
