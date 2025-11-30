079fab9886263be04e7a8dd573e8ed5fb372d4fa
Thibaut Durand <durand.tibo@gmail.com>
2025-11-29 23:58:23 -0800

    Add integration tests for imports (#636)

--- src/feu/testing.py
+++ src/feu/testing.py
@@ -16,9 +16,12 @@ __all__ = [
     "polars_available",
     "pyarrow_available",
     "requests_available",
+    "requests_not_available",
     "scipy_available",
     "sklearn_available",
     "torch_available",
+    "urllib3_available",
+    "urllib3_not_available",
     "uv_available",
     "xarray_available",
 ]
@@ -30,6 +33,7 @@ from feu.imports import (
     is_git_available,
     is_package_available,
     is_requests_available,
+    is_urllib3_available,
 )
 from feu.install import is_pip_available, is_pipx_available, is_uv_available
 
@@ -47,12 +51,19 @@ polars_available = pytest.mark.skipif(not is_package_available("polars"), reason
 pyarrow_available = pytest.mark.skipif(
     not is_package_available("pyarrow"), reason="Requires pyarrow"
 )
-torch_available = pytest.mark.skipif(not is_package_available("torch"), reason="Requires PyTorch")
 requests_available = pytest.mark.skipif(not is_requests_available(), reason="Requires requests")
+requests_not_available = pytest.mark.skipif(
+    is_requests_available(), reason="Skip if requests is available"
+)
 sklearn_available = pytest.mark.skipif(
     not is_package_available("sklearn"), reason="Requires sklearn"
 )
 scipy_available = pytest.mark.skipif(not is_package_available("scipy"), reason="Requires scipy")
+torch_available = pytest.mark.skipif(not is_package_available("torch"), reason="Requires PyTorch")
+urllib3_available = pytest.mark.skipif(not is_urllib3_available(), reason="Requires urllib3")
+urllib3_not_available = pytest.mark.skipif(
+    is_urllib3_available(), reason="Skip if urllib3 is available"
+)
 xarray_available = pytest.mark.skipif(not is_package_available("xarray"), reason="Requires xarray")
 
 
