e03030f8e2eda36d283d7e87d083dc8cb4cb4032
Thibaut Durand <durand.tibo@gmail.com>
2025-11-29 23:45:36 -0800

    Add `is_urllib3_available` (#635)

--- tests/unit/test_imports.py
+++ tests/unit/test_imports.py
@@ -9,9 +9,11 @@ from feu.imports import (
     check_click,
     check_git,
     check_requests,
+    check_urllib3,
     is_click_available,
     is_git_available,
     is_requests_available,
+    is_urllib3_available,
 )
 
 ##########################################
@@ -121,3 +123,25 @@ def test_check_requests_without_package() -> None:
 
 def test_is_requests_available() -> None:
     assert isinstance(is_requests_available(), bool)
+
+
+###################
+#     urllib3     #
+###################
+
+
+def test_check_urllib3_with_package() -> None:
+    with patch("feu.imports.is_urllib3_available", lambda: True):
+        check_urllib3()
+
+
+def test_check_urllib3_without_package() -> None:
+    with (
+        patch("feu.imports.is_urllib3_available", lambda: False),
+        pytest.raises(RuntimeError, match=r"'urllib3' package is required but not installed."),
+    ):
+        check_urllib3()
+
+
+def test_is_urllib3_available() -> None:
+    assert isinstance(is_urllib3_available(), bool)
