632650acacba5ed82799abcbeb3a86125e8f3f10
Thibaut Durand <durand.tibo@gmail.com>
2025-12-10 16:06:06 -0800

    Improve `http` (#679)

--- src/feu/utils/http.py
+++ src/feu/utils/http.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __all__ = ["fetch_data"]
 
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 from feu.imports import (
     check_requests,
@@ -12,13 +12,19 @@ from feu.imports import (
     is_urllib3_available,
 )
 
-if is_requests_available():  # pragma: no cover
+if TYPE_CHECKING:
+    from urllib3.util.retry import Retry
+
+if is_requests_available():
     import requests
     from requests.adapters import HTTPAdapter
+else:  # pragma: no cover
+    from feu.utils.fallback.requests import HTTPAdapter, requests
 
-
-if is_urllib3_available():  # pragma: no cover
+if is_urllib3_available():
     from urllib3.util.retry import Retry
+else:  # pragma: no cover
+    from feu.utils.fallback.urllib3 import Retry
 
 
 def fetch_data(url: str, timeout: float = 10.0, **kwargs: Any) -> dict:
@@ -54,26 +60,26 @@ def fetch_data(url: str, timeout: float = 10.0, **kwargs: Any) -> dict:
     ```
     """
     check_requests()
-    session = requests.Session()  # pyright: ignore[reportPossiblyUnboundVariable]
+    session = requests.Session()
 
     if is_urllib3_available():
-        retry = Retry(  # pyright: ignore[reportPossiblyUnboundVariable]
+        retry = Retry(
             total=5,
             backoff_factor=0.5,
             status_forcelist=[429, 500, 502, 503, 504],
             allowed_methods=["GET"],
             raise_on_status=False,
         )
-        adapter = HTTPAdapter(max_retries=retry)  # pyright: ignore[reportPossiblyUnboundVariable]
+        adapter = HTTPAdapter(max_retries=retry)
         session.mount("https://", adapter)
 
     try:
         resp = session.get(url=url, timeout=timeout, **kwargs)
         resp.raise_for_status()
-    except requests.exceptions.Timeout as exc:  # pyright: ignore[reportPossiblyUnboundVariable]
+    except requests.exceptions.Timeout as exc:
         msg = "GitHub API request timed out"
         raise RuntimeError(msg) from exc
-    except requests.exceptions.RequestException as exc:  # pyright: ignore[reportPossiblyUnboundVariable]
+    except requests.exceptions.RequestException as exc:
         msg = f"Network or HTTP error: {exc}"
         raise RuntimeError(msg) from exc
 
