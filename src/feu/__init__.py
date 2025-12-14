94ab69a38f6710a2b401ef980763737c2be9a743
Thibaut Durand <durand.tibo@gmail.com>
2025-12-14 00:27:57 -0800

    Add `py.typed` and update build workflow (#691)

--- src/feu/__init__.py
+++ src/feu/__init__.py
@@ -11,6 +11,15 @@ __all__ = [
     "is_package_available",
 ]
 
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as metadata_version
+
 from feu.imports import is_module_available, is_package_available
 from feu.install import install_package, install_package_closest_version
 from feu.version import compare_version, get_package_version
+
+try:
+    __version__ = metadata_version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    # Package is not installed, fallback if needed
+    __version__ = "0.0.0"
