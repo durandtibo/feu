b0770ee1f5d04eda4856fe9aaed652217a73ca4c
Thibaut Durand <durand.tibo@gmail.com>
2025-12-28 20:29:53 -0800

    Add `fetch_github_repos`

--- src/feu/repo/__init__.py
+++ src/feu/repo/__init__.py
@@ -2,6 +2,10 @@ r"""Contain functions to manage repos."""
 
 from __future__ import annotations
 
-__all__ = ["fetch_github_metadata"]
+__all__ = ["display_repos_summary", "fetch_github_metadata", "fetch_github_repos"]
 
-from feu.repo.github import fetch_github_metadata
+from feu.repo.github import (
+    display_repos_summary,
+    fetch_github_metadata,
+    fetch_github_repos,
+)
