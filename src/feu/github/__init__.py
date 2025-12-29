cda6befd71ef26c962237bf434ee7cc1180a16f4
Thibaut Durand <durand.tibo@gmail.com>
2025-12-29 00:37:36 -0800

    Refactor code (#712)

---
+++ src/feu/github/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain GitHub utility functions."""
+
+from __future__ import annotations
+
+__all__ = ["display_repos_summary", "fetch_github_repos"]
+
+from feu.github.repos import display_repos_summary, fetch_github_repos
