811036b6ab38ac4fe73005772a301f839498587e
Thibaut Durand <durand.tibo@gmail.com>
2025-12-29 01:20:49 -0800

    Add `sort_repos_by_name` (#714)

--- src/feu/github/__init__.py
+++ src/feu/github/__init__.py
@@ -2,6 +2,7 @@ r"""Contain GitHub utility functions."""
 
 from __future__ import annotations
 
-__all__ = ["display_repos_summary", "fetch_github_repos"]
+__all__ = ["display_repos_summary", "fetch_github_repos", "sort_repos_by_name"]
 
 from feu.github.repos import display_repos_summary, fetch_github_repos
+from feu.github.sorting import sort_repos_by_name
