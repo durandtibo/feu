55e70d3a5025554b21d67f2d2326227506a6ec09
Thibaut Durand <durand.tibo@gmail.com>
2026-06-25 15:06:31 -0700

    Add `check_package`

---
+++ src/feu/utils/imports/__init__.py
@@ -0,0 +1,19 @@
+r"""Contain utilities to manage optional dependencies."""
+
+from __future__ import annotations
+
+__all__ = [
+    "check_package",
+    "decorator_package_available",
+    "module_available",
+    "package_available",
+    "raise_package_missing_error",
+]
+
+from feu.utils.imports.universal import (
+    check_package,
+    decorator_package_available,
+    module_available,
+    package_available,
+    raise_package_missing_error,
+)
