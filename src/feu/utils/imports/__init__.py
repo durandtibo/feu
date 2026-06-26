4a435551aeeb00ffc3bb7bd5ace9a29fa7f21c70
Thibaut Durand <durand.tibo@gmail.com>
2026-06-25 21:47:24 -0700

    Update `is_requests_available`

--- src/feu/utils/imports/__init__.py
+++ src/feu/utils/imports/__init__.py
@@ -6,16 +6,20 @@ __all__ = [
     "check_click",
     "check_git",
     "check_package",
+    "check_requests",
     "click_available",
     "decorator_package_available",
     "git_available",
     "is_click_available",
     "is_git_available",
+    "is_requests_available",
     "module_available",
     "package_available",
     "raise_click_missing_error",
     "raise_git_missing_error",
     "raise_package_missing_error",
+    "raise_requests_missing_error",
+    "requests_available",
 ]
 
 from feu.utils.imports.click import (
@@ -29,6 +33,12 @@ from feu.utils.imports.git import (
     git_available,
     is_git_available,
     raise_git_missing_error,
+)
+from feu.utils.imports.requests import (
+    check_requests,
+    is_requests_available,
+    raise_requests_missing_error,
+    requests_available,
 )
 from feu.utils.imports.universal import (
     check_package,
