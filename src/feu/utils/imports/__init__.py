700687626d2c4507e4b62f2e494e720cf6e9ca25
Thibaut Durand <durand.tibo@gmail.com>
2026-06-25 21:40:07 -0700

    Update `is_click_available`

--- src/feu/utils/imports/__init__.py
+++ src/feu/utils/imports/__init__.py
@@ -3,14 +3,23 @@ r"""Contain utilities to manage optional dependencies."""
 from __future__ import annotations
 
 __all__ = [
+    "check_click",
     "check_package",
+    "click_available",
     "decorator_package_available",
+    "is_click_available",
     "module_available",
     "package_available",
+    "raise_click_missing_error",
     "raise_package_missing_error",
 ]
 
-
+from feu.utils.imports.click import (
+    check_click,
+    click_available,
+    is_click_available,
+    raise_click_missing_error,
+)
 from feu.utils.imports.universal import (
     check_package,
     decorator_package_available,
