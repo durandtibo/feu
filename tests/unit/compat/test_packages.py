d9e6031bc0a3a0fba2d6c3284f9ab26ca4f91855
Thibaut Durand <durand.tibo@gmail.com>
2026-08-02 10:28:46 -0700

    Rename defaults (#894)

--- tests/unit/compat/test_packages.py
+++ tests/unit/compat/test_packages.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from feu.compat.defaults import DEFAULT_COMPAT
 from feu.compat.packages import get_package_names
 
 #############################################
@@ -10,10 +9,6 @@ from feu.compat.packages import get_package_names
 
 def test_get_package_names_returns_list() -> None:
     assert isinstance(get_package_names(), list)
-
-
-def test_get_package_names_matches_default_compat_keys() -> None:
-    assert get_package_names() == list(DEFAULT_COMPAT.keys())
 
 
 def test_get_package_names_contains_expected_packages() -> None:
